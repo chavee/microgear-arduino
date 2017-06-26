e3e4fcf096a36ec052a1c4e7f97d8d13cc759b44
Chavee Issariyapat <i@chavee.com>
2017-06-26 13:17:26 +0700

    microgear first working version

--- src/Microgear.cpp
+++ src/Microgear.cpp
@@ -1,14 +1,49 @@
 #include "Microgear.h"
 
-void messageReceived(String topic, String payload, char * bytes, unsigned int length) {
-  Serial.print("incoming --> ");
-  Serial.print(topic);
-  Serial.print(" : ");
-  Serial.print(payload);
-  Serial.println();
+Microgear *mg;
+char pbuffer[MAXPAYLOADSIZE+1];
+unsigned int topicprefixlen = 0;
+
+void messageReceived(char *topic, uint8_t *payload, unsigned int length) {
+     char* rtopic =  topic+topicprefixlen+1;
+
+     if (*rtopic == '&') {
+         if (strcmp(rtopic,"&present") == 0) {
+             if (mg->cb_present) {
+                 mg->cb_present("present",payload,length);
+             }
+         }
+         else if (strcmp(rtopic,"&absent") == 0) {
+             if (mg->cb_absent) {
+                 mg->cb_absent("absent",payload,length);
+             }
+         }
+         else if (strcmp(rtopic,"&resetendpoint") == 0) {
+             #ifdef DEBUG_H
+                 Serial.println("to reset endpoint");
+             #endif
+//             if (mg) mg->resetEndpoint();
+         }
+     }
+     else if (*topic == '@') {
+         if (strcmp(topic,"@error")==0) {
+             if (mg->cb_error)  {
+                 mg->cb_error("error",payload,length);
+             }
+         }
+         else if (strcmp(topic,"@info")==0) {
+             if (mg->cb_info)  {
+                 mg->cb_info("info",payload,length);
+             }
+         }
+     }
+     else if (mg->cb_message) {
+         mg->cb_message(topic,payload,length);
+     }
 }
 
 Microgear::Microgear(Client& tcpclient ) {
+  mg = this;
   this->client = &tcpclient;
 }
 
@@ -18,29 +53,54 @@ void Microgear::init(char *key, char *secret, char *alias) {
     this->token = NULL;
     this->tokensecret = NULL;
     this->alias = alias;
-/*
-    mg->cb_connected = NULL;
-    mg->cb_absent = NULL;
-    mg->cb_present = NULL;
-    mg->cb_message = NULL;
-    mg->cb_error = NULL;
-    mg->cb_info = NULL;
-*/
+
+    this->cb_connected = NULL;
+    this->cb_absent = NULL;
+    this->cb_present = NULL;
+    this->cb_message = NULL;
+    this->cb_error = NULL;
+    this->cb_info = NULL;
 }
 
-
-int Microgear::connect(char*) {
+int Microgear::connect(char* appid) {
+  char *p;
   char mqtt_username[MQTT_USERNAME_SIZE+1];
   char raw_password[20];
   char mqtt_password[MQTT_PASSWORD_SIZE+1];
   char hashkey[SECRETSIZE+TOKENSECRETSIZE+2];
 
+  this->appid = appid;
+  topicprefixlen = strlen(appid)+1;
   this->mqttclient.begin(this->host, this->port,*(this->client));
 
+  if (this->token) strcpy(tokenstore.token, this->token);
+  if (this->tokensecret) strcpy(tokenstore.secret, this->tokensecret);
 
+  strrep(mqtt_username, tokenstore.token);
+  p = addattr(mqtt_username+strlen(tokenstore.token), "%", this->key);
+//  addattr(p, "%", getTimeStr());
+  addattr(p, "%", "1498324944");
+  strrep(hashkey, tokenstore.secret);
+  addattr(hashkey+strlen(tokenstore.secret), "&", this->secret);
 
+  Sha1.initHmac((uint8_t*)hashkey,strlen(hashkey));
+  Sha1.HmacBase64(mqtt_password,mqtt_username);
 
-  return 0;
+  #ifdef DEBUG
+      Serial.println("Going to connect to MQTT broker");
+      Serial.println(tokenstore.token);
+      Serial.println(mqtt_username);
+      Serial.println(mqtt_username+strlen(tokenstore.token)+1);
+      Serial.println(mqtt_password);
+      Serial.println(hashkey);
+  #endif
+
+  int ret = this->mqttclient.connect(tokenstore.token, mqtt_username+strlen(tokenstore.token)+1, mqtt_password);
+  return ret == MQTT::SUCCESS;
+}
+
+bool Microgear::connected() {
+    return this->mqttclient.connected();
 }
 
 void Microgear::setToken(char* token, char* tokensecret, char* endpoint) {
@@ -56,4 +116,128 @@ void Microgear::setToken(char* token, char* tokensecret, char* endpoint) {
         this->port = atoi(p+1);
     }
   }
+}
+
+void Microgear::on(unsigned char event, CALLBACK callback) {
+    switch (event) {
+        case MESSAGE :
+                if (callback) cb_message = callback;
+                break;
+        case PRESENT :
+                if (callback) cb_present = callback;
+                if (connected())
+                    subscribe("/&present");
+                break;
+        case ABSENT :
+                if (callback) cb_absent = callback;
+                if (connected())
+                    subscribe("/&absent");
+                break;
+        case CONNECTED :
+                if (callback) cb_connected = callback;
+                break;
+        case ERROR :
+                if (callback) cb_error = callback;
+                break;
+        case INFO :
+                if (callback) cb_info = callback;
+                break;
+    }
+}
+
+void Microgear::subscribe(char* topic) {
+    char top[MAXTOPICSIZE] = "/";
+
+    strcat(top,appid);
+    strcat(top,topic);
+    mqttclient.subscribe(top);
+}
+
+void Microgear::unsubscribe(char* topic) {
+    char top[MAXTOPICSIZE] = "/";
+
+    strcat(top,appid);
+    strcat(top,topic);
+    mqttclient.unsubscribe(top);
+}
+
+void Microgear::loop() {
+  this->mqttclient.loop();
+}
+
+bool Microgear::publish(char* topic, char* message, bool retained) {
+    char top[MAXTOPICSIZE] = "/";
+    addattr(top+1, this->appid, topic);
+    return this->mqttclient.publish(top, message, retained);
+}
+
+bool Microgear::publish(char* topic, char* message) {
+    return publish(topic, message, false);
+}
+
+bool Microgear::publish(char* topic, double message, int n) {
+    return publish(topic, message, n, false);
+}
+
+bool Microgear::publish(char* topic, double message, int n, bool retained) {
+    char mstr[16];
+    dtostrf(message,0,n,mstr);
+    return publish(topic, mstr, retained);
+}
+
+bool Microgear::publish(char* topic, double message) {
+    return publish(topic, message, 8, false);
+}
+
+bool Microgear::publish(char* topic, double message, bool retained) {
+    return publish(topic, message, 8, retained);
+}
+
+bool Microgear::publish(char* topic, int message) {
+    return publish(topic, message, 0, false);
+}
+
+bool Microgear::publish(char* topic, int message, bool retained) {
+    return publish(topic, message, 0, retained);
+}
+
+bool Microgear::publish(char* topic, String message) {
+    return publish(topic, message, false);
+}
+
+bool Microgear::publish(char* topic, String message, bool retained) {
+    message.toCharArray(pbuffer,MAXPAYLOADSIZE-1);
+    return publish(topic, pbuffer, retained);
+}
+
+bool Microgear::chat(char* target, char* message) {
+    bool result;
+    char chattopic[MAXALIASSIZE+11];
+    addattr(chattopic,"/gearname/",target);
+    result = this->publish(chattopic, message);
+    this->mqttclient.loop();
+    return result;
+}
+
+bool Microgear::chat(char* topic, double message, int n) {
+    bool result;
+    char mstr[16];
+
+    dtostrf(message,0,n,mstr);
+    result = chat(topic, mstr);
+    this->mqttclient.loop();
+    return result;
+}
+
+bool Microgear::chat(char* topic, double message) {
+    return chat(topic, message, 8);
+}
+
+bool Microgear::chat(char* topic, int message) {
+    return chat(topic, message, 0);
+}
+
+bool Microgear::chat(char* topic, String message) {
+    message.toCharArray(pbuffer,MAXPAYLOADSIZE);
+    return chat(topic, pbuffer);
 }
--- src/Microgear.h
+++ src/Microgear.h
@@ -5,6 +5,9 @@
 #include <ESP8266WiFi.h>
 #include "MQTTClient/MQTTClient.h"
 #include "SHA1.h"
+#include "func.h"
+#include "TokenStore.h"
+#include "config.h"
 
 #define APPIDSIZE                  32
 #define KEYSIZE                    16
@@ -20,14 +23,28 @@
 #define PUB_MSG_LEN                16
 #define MAXALIASSIZE               16
 
+#define MAXTOPICSIZE               128
+#define MAXPAYLOADSIZE             128
+
+
 #define MQTT_USERNAME_SIZE         TOKENSIZE+KEYSIZE+13
 #define MQTT_PASSWORD_SIZE         33
 
+/* Event Type */
+#define MESSAGE                    1
+#define PRESENT                    2
+#define ABSENT                     3
+#define CONNECTED                  4
+#define ERROR          		         5
+#define INFO                  	   6
+
+typedef void (* CALLBACK)(char*, uint8_t*, unsigned int);
 
 class Microgear {
   private:
     MQTTClient mqttclient;
     Client *client;
+    Token tokenstore;
 
   public:
     char *appid;
@@ -40,10 +57,44 @@ class Microgear {
     char *host;
     uint16 port;
 
+    CALLBACK cb_connected;
+    CALLBACK cb_absent;
+    CALLBACK cb_present;
+    CALLBACK cb_message;
+    CALLBACK cb_error;
+    CALLBACK cb_info;
+
     Microgear(Client&);
     void init(char*, char*, char*);
     void setToken(char*, char*, char*);
     int connect(char*);
+    void loop();
+    bool connected();
+
+		bool publish(char*, char*);
+		bool publish(char*, char*, bool);
+    bool publish(char*, double);
+		bool publish(char*, double, bool);
+		bool publish(char*, double, int);
+		bool publish(char*, double, int, bool);
+		bool publish(char*, int);
+		bool publish(char*, int, bool);
+		bool publish(char*, String);
+		bool publish(char*, String, bool);
+		bool publish(char*, String, String);
+
+    void subscribe(char*);
+		void unsubscribe(char*);
+
+    bool chat(char*, char*);
+		bool chat(char*, int);
+		bool chat(char*, double);
+		bool chat(char*, double, int);
+		bool chat(char*, String);
+
+    void on(unsigned char event, CALLBACK);
+
 };
 
+typedef Microgear MicroGear;
 #endif
