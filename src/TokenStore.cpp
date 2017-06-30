38be7c15f05e10950389c079f949987837beec33
XezXey <puntawat-1@hotmail.com>
2017-06-30 14:57:31 +0700

    add getChecksum and compareChecksum

--- src/TokenStore.cpp
+++ src/TokenStore.cpp
@@ -1,16 +1,53 @@
 #include "TokenStore.h"
+#include "Arduino.h"
+extern "C" {
+  #include "spi_flash.h"
+}
+extern "C" uint32_t _SPIFFS_end;
+
+uint32_t ESP_FLASH_SEC = ((_SPIFFS_end - 0x40200000) / SPI_FLASH_SEC_SIZE) + 1;
 
 void saveToken(Token *token) {
-//    spi_flash_erase_sector(ESP_FLASH_SEC);
-//    spi_flash_write(ESP_FLASH_SEC * SPI_FLASH_SEC_SIZE, (uint32 *)token, sizeof(Token));
+    spi_flash_erase_sector(ESP_FLASH_SEC);
+    spi_flash_write(ESP_FLASH_SEC * SPI_FLASH_SEC_SIZE, (uint32_t *)token, sizeof(Token));
+    saveChecksum(token);
 }
 
-void loadToken(Token *token) {
-//    memset(token, 0, sizeof(Token));
-//    spi_flash_read(ESP_FLASH_SEC * SPI_FLASH_SEC_SIZE, (uint32 *)token, sizeof(Token));
+bool loadToken(Token *token) {
+    Token* dummyToken;
+    memset(token, 0, sizeof(dummyToken));
+    spi_flash_read(ESP_FLASH_SEC * SPI_FLASH_SEC_SIZE, (uint32_t *)dummyToken, sizeof(Token));
+
+    if(compareChecksum(dummyToken)){
+      memset(token, 0, sizeof(dummyToken));
+      spi_flash_read(ESP_FLASH_SEC * SPI_FLASH_SEC_SIZE, (uint32_t *)token, sizeof(Token));
+      return true;
+    }
+    else false;
 }
 
 void clearToken(Token *token) {
-//    memset(token, 0, sizeof(Token));
-//    spi_flash_erase_sector(ESP_FLASH_SEC);
+    memset(token, 0, sizeof(Token));
+    spi_flash_erase_sector(ESP_FLASH_SEC);
+}
+
+uint32_t generateChecksum(uint8_t *token, uint16_t data_len) {
+    uint32_t checksum = 0;
+    while(data_len--) {
+        checksum += *(token);
+        token++;
+    }
+    return checksum;
+}
+
+void saveChecksum(Token *token) {
+    uint16_t data_len = sizeof(Token) - sizeof(token->checksum);
+    token->checksum = generateChecksum((uint8_t*)token, data_len);
+}
+
+bool compareChecksum(Token *token) {
+    uint16_t data_len =  sizeof(Token) - sizeof(token->checksum);
+    if(token->checksum == generateChecksum((uint8_t*)token, data_len))
+        return true;
+    else return false;
 }
--- src/TokenStore.h
+++ src/TokenStore.h
@@ -1,31 +1,34 @@
 #ifndef TOKENSTORE_H
 #define TOKENSTORE_H
-
-#include <arduino.h>
+#include "stdint.h"
+#include "Arduino.h"
 
 #define KEYSIZE                  16
 #define TOKENSIZE                16
 #define TOKENSECRETSIZE          32
 #define ENDPOINTSIZE             200
-#define REVOKECODESIZE			 28
+#define REVOKECODESIZE           28
 
 struct token_struct{
-    char type;
-    char key[KEYSIZE+1];
-    char token[TOKENSIZE+1];
-    char secret[TOKENSECRETSIZE+1];
-    char saddr[ENDPOINTSIZE+1];
-    uint16_t sport;
-    char flag;
-    char revokecode[REVOKECODESIZE+1];
-    char checksum[2];
-    char dummy[1];			// make struct size devidable by 4
+  char type;
+  char key[KEYSIZE+1];
+  char token[TOKENSIZE+1];
+  char secret[TOKENSECRETSIZE+1];
+  char saddr[ENDPOINTSIZE+1];
+  uint16_t sport;
+  char flag;
+  char revokecode[REVOKECODESIZE+1];
+  uint32_t checksum;
 };
-
 typedef struct token_struct Token;
 
+uint32_t generateChecksum(uint8_t*, uint16_t);
+bool compareChecksum(Token*);
+void showAllData(uint8_t* token);
+void saveChecksum(Token*);
 void saveToken(Token*);
-void loadToken(Token*);
+bool loadToken(Token*);
 void clearToken(Token*);
 
 #endif
+
